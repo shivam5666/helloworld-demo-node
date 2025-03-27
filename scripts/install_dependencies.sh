2fe97e91f22050c06db693f298852deef8c5de5d
EC2 Default User <ec2-user@ip-10-0-7-82.ap-south-1.compute.internal>
2025-03-27 06:36:32 +0000

    fifth

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Installing dependencies..."
 sudo chown -R ec2-user:ec2-user /home/ec2-user/nextjs-app
+
+# Ensure NVM is loaded
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Check npm
+which npm || { echo "npm not found"; exit 1; }
 cd /home/ec2-user/nextjs-app
 npm install
 npm install pm2 -g
