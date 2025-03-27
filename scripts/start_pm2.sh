3db1fa8090c706a872cfe02e49566b5d5d00f254
EC2 Default User <ec2-user@ip-10-0-7-82.ap-south-1.compute.internal>
2025-03-27 06:40:52 +0000

    sixth

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "Starting Next.js application with PM2..."
+# Ensure NVM is loaded
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Ensure pm2 is installed (in case it's not)
+which pm2 || { echo "pm2 not found, installing..."; npm install -g pm2; }
+
+# Start the application using pm2
+echo "Starting Next.js application with PM2..."
+
 cd /home/ec2-user/nextjs-app
 
 # Stop previous process
